1a978a516a6956fb4dec76fe545eea17194dd04e
Pedro Alexandre Romano Lopes <pedroalexlopes25@gmail.com>
2020-06-05 23:24:00 +0100

    Insert and remove items from List

--- src/TopPopulations/Program.cs
+++ src/TopPopulations/Program.cs
@@ -10,8 +10,12 @@ namespace TopPopulations
         {
             var reader = new CsvReader2(CSV_FILE_PATH);
 
-            Country[] countries = reader.ReadFirstNCountries(500);
-            //List<Country> countries = reader.ReadAllCountries();
+            //Country[] countries = reader.ReadFirstNCountries(10);
+            List<Country> countries = reader.ReadAllCountries();
+            Country lillliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            int lilliputIndex = countries.FindIndex(x => x.Population < 2_000_000);
+            countries.Insert(lilliputIndex, lillliput);
+            countries.RemoveAt(lilliputIndex);
 
             foreach (Country country in countries)
                 Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
