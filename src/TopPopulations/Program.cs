982b1044bd8d31cc00c17765ba105733f3ebda9c
Pedro Alexandre Romano Lopes <pedroalexlopes25@gmail.com>
2020-06-06 10:52:14 +0100

    Using LINQ to take top 10 countries of a picked region

--- src/TopPopulations/Program.cs
+++ src/TopPopulations/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TopPopulations
 {
@@ -11,27 +12,50 @@ namespace TopPopulations
             var reader = new CsvReader2(CSV_FILE_PATH);
 
 
-            Console.WriteLine("Write country code to lookup");
+            /*
+                Console.WriteLine("Write country code to lookup");
+                var input = Console.ReadLine();
+                var countries = reader.ReadAllCountriesDict();
+                var country = countries.GetValueOrDefault(input);
+                if (country == null)
+
+                    Console.WriteLine($"No country information found for country code {input}");
+                else
+                    Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}, Country Code: {country.Code}");
+            */
+
+            // Groupd countries by region
+            List<Country> countries = reader.ReadAllCountries();
+            var countriesByRegion = new Dictionary<string, List<string>>();
+            countries.ForEach(c =>
+            {
+                var regionCountries = countriesByRegion.GetValueOrDefault(c.Region);
+                if (regionCountries == null)
+                {
+                    regionCountries = new List<string>();
+                    countriesByRegion.Add(c.Region, regionCountries);
+                }
+                regionCountries.Add(c.Name);
+            });
+
+            // Print region followed by its countries
+            countriesByRegion.ToList().ForEach(kv =>
+            {
+                Console.WriteLine($"Region: {kv.Key}");
+                kv.Value.ForEach(c => Console.WriteLine($"Country: {c}"));
+            });
+
+            Console.WriteLine("Select region whose top 10 countries should be printed");
             var input = Console.ReadLine();
 
-
-            var countries = reader.ReadAllCountriesDict();
-            var country = countries.GetValueOrDefault(input);
-            if (country == null)
-
-                Console.WriteLine($"No country information found for country code {input}");
-            else
-                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}, Country Code: {country.Code}");
-
-
-            /*
-            Country[] countries = reader.ReadFirstNCountries(10);
-            foreach (var country in countries)
+            var regionCountries = countriesByRegion.GetValueOrDefault(input);
+            if (regionCountries == null)
             {
-                var key = country.Key;
-                var value = country.Value;
-                Console.WriteLine($"{PopulationFormatter.FormatPopulation(value.Population).PadLeft(15)}: {value.Name}, Country Code: {key}");
-            }*/
+                Console.WriteLine("Region not found");
+                return;
+            }
+
+            regionCountries.Take(10).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
